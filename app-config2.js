b5e0860ab0e79d6319472dfbd25bb6fff7d33cc3
Zetx <user@domain>
2019-07-10 20:09:54 -0400

    update test key

--- app-config2.js
+++ app-config2.js
@@ -1,4 +1,4 @@
-var clientID = 'qfz97qvzhnhio4agwc80cuni7q7xx6';
+var clientID = 't2lcmeekc9o7mf1dh0c6l7ieqnz0d4';
 var redirectURL = 'https://tc.zx3.org/index2.html';
 
 var twitchApiBaseUrl = 'https://api.twitch.tv/v5/';
@@ -8,4 +8,4 @@ var twitchApiBaseHeaders = new Headers({
     'Authorization': 'OAuth ' + localStorage.token,
     'Content-Type': 'application/json'
 });
-var twitchApiAuthUrl = 'https://api.twitch.tv/kraken/oauth2/authorize?client_id=' + clientID + '&redirect_uri=' + encodeURIComponent(redirectURL) + '&response_type=token&scope=channel:moderate+chat:edit+chat:read+whispers:read+whispers:edit';
+var twitchApiAuthUrl = 'https://api.twitch.tv/kraken/oauth2/authorize?client_id=' + clientID + '&redirect_uri=' + encodeURIComponent(redirectURL) + '&response_type=token&scope=channel:moderate+chat:edit+chat:read+whispers:read+whispers:edit+chat_login';
